b643e156bb3fe582ce63d0f07dd18857294f8cc5
Annajini <137381616+Annajini@users.noreply.github.com>
2025-07-07 14:02:32 +0530

    Create Stripepayementcontroller.php

---
+++ Stripepayementcontroller.php
@@ -0,0 +1,28 @@
+// app/Http/Controllers/StripePaymentController.php
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Stripe\Stripe;
+use Stripe\Charge;
+
+class StripePaymentController extends Controller
+{
+    public function index()
+    {
+        return view('payment');
+    }
+
+    public function makePayment(Request $request)
+    {
+        Stripe::setApiKey(env('STRIPE_SECRET'));
+
+        Charge::create([
+            'amount' => 100 * 100, // 100 USD in cents
+            'currency' => 'usd',
+            'source' => $request->stripeToken,
+            'description' => 'Test payment from Laravel app',
+        ]);
+
+        return back()->with('success', 'Payment successful!');
+    }
+}
